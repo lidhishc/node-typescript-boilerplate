47c07b634fe72f6e421ec10ccd67df98e5e4c838
lidhishc <lidhishc@gmail.com>
2022-05-20 20:31:56 +0530

    fix: space

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 3004;
 require('../env/validateEnv');
 
 app.listen(PORT, () => {
-  console.log('server is running on port ', PORT);
+  console.log('server is running on port', PORT);
 });
